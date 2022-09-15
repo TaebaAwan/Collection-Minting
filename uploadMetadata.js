a93b883e3bd6281322adc602f44ebe42d99489d3
TaebaAwan <100029495+TaebaAwan@users.noreply.github.com>
2022-09-15 05:17:50 +0500

    Create uploadMetadata.js

---
+++ uploadMetadata.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const {PinImageToIpfs} = require("./pinataApi");
+const ImagePath = path.join(__dirname, "../Images");
+const fs = require("fs");
+
+const PinToPinata = async () => {
+  const images = fs.readdirSync(ImagePath);
+  console.log(images)
+
+  for (const image of images) {
+    await PinImageToIpfs(ImagePath, image); 
+  }
+};
+
+PinToPinata();
